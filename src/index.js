4838626f4788c4e1976207d06dfa4c2828e1f427
John Lindquist <johnlindquist@Johns-MacBook-Pro.local>
2019-12-16 15:20:38 -0700

    iterators

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const abcs = ["A", "B", "C"]
+
+const createIterator = abcs[Symbol.iterator].bind(abcs)
+
+const iterator = createIterator()
+
+for (const i of iterator) {
+    console.log(i)
+}
+
+
+
+
